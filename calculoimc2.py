67da25ac3d99c8f30a117fa229b48f1d86797815
Leonardo Matheus de Oliveira <leomatheusoliveira5@gmail.com>
2023-10-19 15:14:22 -0300

    Calculadora IMC 2.0

---
+++ calculoimc2.py
@@ -0,0 +1,11 @@
+peso = int(input("Digite seu peso:\n"))
+altura = input("Digite sua altura em metros:\n")
+imc = peso / (altura * altura)
+if imc < 18.5:
+  print(f"Seu IMC é {imc}, você está abaixo do peso.")
+elif imc < 25:
+  print(f"Seu IMC é {imc}, você está no peso normal.")
+elif imc < 30:
+  print(f"Seu IMC é {imc}, você está um pouco acima do peso.")
+elif imc < 35:
+  print(f"Seu IMC é {imc}, você está obeso.")
