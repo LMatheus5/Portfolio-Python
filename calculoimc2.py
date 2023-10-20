8174dc7d19209f81b6cf372ae684c722843184a6
Leonardo Matheus de Oliveira <leomatheusoliveira5@gmail.com>
2023-10-20 20:12:56 -0300

    Update calculoimc2.py

--- calculoimc2.py
+++ calculoimc2.py
@@ -1,5 +1,5 @@
 peso = int(input("Digite seu peso:\n"))
-altura = input("Digite sua altura em metros:\n")
+altura = float(input("Digite sua altura em metros:\n"))
 imc = peso / (altura * altura)
 if imc < 18.5:
   print(f"Seu IMC é {imc}, você está abaixo do peso.")
